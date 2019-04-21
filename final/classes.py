b8bd2ecb3a542309297307a5f3fffc9e8b5f91c6
byt411 <byt411@hotmail.com>
2019-04-21 01:30:15 +0100

    completed program by combining multipliers from 2015 and 2016. surveys for final are now inputted manually using addsurveys.

---
+++ final/classes.py
@@ -0,0 +1,140 @@
+import operator
+class Survey:
+    def __init__(self, row):
+        self.pp = row[0].rstrip()
+        self.psoe = row[1].rstrip()
+        self.up = row[2].rstrip()
+        self.cs = row[3].rstrip()
+        self.vox = row[4].rstrip()
+
+
+pp15Multi = 28.72 / 28.1
+pp18Multi = 20.7 / 19.7
+psoe15Multi = 22.01 / 23.9
+psoe18Multi = 27.9 / 30.6
+up15Multi = 20.66 / 15.3
+up18Multi = 16.2 / 18.4
+cs15Multi = 13.93 / 18.6
+cs18Multi = 18.3 / 17.6
+voxMulti1 = 11.0 / 10.2
+voxMulti2 = 11.0 / 8.7
+
+class Results:
+    def __init__(self, array):
+        self.array = array
+        self.pp = []
+        self.psoe = []
+        self.up = []
+        self.cs = []
+        self.vox = []
+        self.parties = [self.pp, self.psoe, self.up, self.cs, self.vox]
+        for survey in self.array:
+            self.pp.append(float(survey.pp))
+            self.psoe.append(float(survey.psoe))
+            self.up.append(float(survey.up))
+            self.cs.append(float(survey.cs))
+            self.vox.append(float(survey.vox))
+
+        veryrecent = sum(self.pp[0:6]) / float(5)
+        recent = sum(self.pp[6:11]) / float(5)
+        medium = sum(self.pp[11:26]) / float(15)
+        old = sum(self.pp[26:]) / float(len(array) - 25)
+        estimate = (veryrecent * 0.25) + (recent * 0.20) + (medium * 0.35) + (old * 0.20)
+        adjustedestimate = estimate * ((0.55 * pp15Multi) + (0.45 * pp18Multi))
+        self.pp = adjustedestimate
+        self.ppstring = str(round(self.pp, 2)) + "%"
+
+        veryrecent = sum(self.psoe[0:6]) / float(5)
+        recent = sum(self.psoe[6:11]) / float(5)
+        medium = sum(self.psoe[11:26]) / float(15)
+        old = sum(self.psoe[26:]) / float(len(array) - 25)
+        estimate = (veryrecent * 0.25) + (recent * 0.20) + (medium * 0.35) + (old * 0.20)
+        adjustedestimate = estimate * ((0.55 * psoe15Multi) + (0.45 * psoe18Multi))
+        self.psoe = adjustedestimate
+        self.psoestring = str(round(self.psoe, 2)) + "%"
+
+        veryrecent = sum(self.up[0:6]) / float(5)
+        recent = sum(self.up[6:11]) / float(5)
+        medium = sum(self.up[11:26]) / float(15)
+        old = sum(self.up[26:]) / float(len(array) - 25)
+        estimate = (veryrecent * 0.25) + (recent * 0.20) + (medium * 0.35) + (old * 0.20)
+        adjustedestimate = estimate * ((0.55 * up15Multi) + (0.45 * up18Multi))
+        self.up = adjustedestimate
+        self.upstring = str(round(self.up, 2)) + "%"
+
+        veryrecent = sum(self.cs[0:6]) / float(5)
+        recent = sum(self.cs[6:11]) / float(5)
+        medium = sum(self.cs[11:26]) / float(15)
+        old = sum(self.cs[26:]) / float(len(array) - 25)
+        estimate = (veryrecent * 0.25) + (recent * 0.20) + (medium * 0.35) + (old * 0.20)
+        adjustedestimate = estimate * ((0.55 * cs15Multi) + (0.45 * cs18Multi))
+        self.cs = adjustedestimate
+        self.csstring = str(round(self.cs, 2)) + "%"
+
+        veryrecent = sum(self.vox[0:6]) / float(5)
+        recent = sum(self.vox[6:11]) / float(5)
+        medium = sum(self.vox[11:26]) / float(15)
+        old = sum(self.vox[26:]) / float(len(array) - 25)
+        estimate = (veryrecent * 0.25) + (recent * 0.20) + (medium * 0.35) + (old * 0.20)
+        adjustedestimate = estimate * ((0.55 * voxMulti1) + (0.45 * voxMulti2))
+        self.vox = adjustedestimate
+        self.voxstring = str(round(self.vox, 2)) + "%"
+
+class Province:
+        def __init__(self, line, percentages):
+            self.name = line[0]
+            self.seats = int(line[1])
+            self.ppmulti = float(line[2])
+            self.psoemulti = float(line[3])
+            self.upmulti = float(line[4])
+            self.csmulti = float(line[5])
+            self.voxmulti = float((self.ppmulti + self.csmulti) / 2)
+
+            self.pp = self.ppmulti * percentages[0]
+            self.psoe = self.psoemulti * percentages[1]
+            self.up = self.upmulti * percentages[2]
+            self.cs = self.csmulti * percentages[3]
+            self.vox = self.voxmulti * percentages[4]
+            other = 95 - self.pp - self.psoe - self.up - self.cs - self.vox
+            self.other = other
+
+            self.ppseats = 0
+            self.psoeseats = 0
+            self.upseats = 0
+            self.csseats = 0
+            self.voxseats = 0
+            self.otherseats = 0
+
+            self.seatcount = 1
+            self.seatlist = []
+            while self.seatcount - 1 < self.seats:
+                seatentry = ('PP', (self.pp / self.seatcount))
+                self.seatlist.append(seatentry)
+                seatentry = ('PSOE', (self.psoe / self.seatcount))
+                self.seatlist.append(seatentry)
+                seatentry = ('UP', (self.up / self.seatcount))
+                self.seatlist.append(seatentry)
+                seatentry = ('Cs', (self.cs / self.seatcount))
+                self.seatlist.append(seatentry)
+                seatentry = ('Vox', (self.vox / self.seatcount))
+                self.seatlist.append(seatentry)
+                seatentry = ('Other', (self.other / self.seatcount))
+                self.seatlist.append(seatentry)
+                self.seatcount += 1
+            self.seatlist.sort( key = operator.itemgetter(1), reverse = True)
+            self.seatlist = self.seatlist[0:self.seats]
+
+            for seat in self.seatlist:
+                if seat[0] == 'PP':
+                    self.ppseats += 1
+                elif seat[0] == 'PSOE':
+                    self.psoeseats += 1
+                elif seat[0] == 'UP':
+                    self.upseats += 1
+                elif seat[0] == 'Cs':
+                    self.csseats += 1
+                elif seat[0] == 'Vox':
+                    self.voxseats += 1
+                elif seat[0] == 'Other':
+                    self.otherseats += 1
+            print self.name + ' ' + str(self.ppseats) + ' ' + str(self.psoeseats) + ' ' + str(self.upseats) + ' ' + str(self.csseats) + ' ' + str(self.voxseats) + ' ' + str(self.otherseats)
---
+++ final/results.py
@@ -0,0 +1,53 @@
+import requests
+import statistics
+
+from classes import Survey, Results, Province
+
+
+
+surveyfile = open("surveys", "r")
+surveyRaw = surveyfile.readlines()
+surveys = []
+for row in surveyRaw:
+    surveydata = row.rstrip().split()
+    newSurvey = Survey(surveydata)
+    surveys.append(newSurvey)
+
+results = Results(surveys)
+percentages = [results.pp, results.psoe, results.up, results.cs, results.vox]
+print '-----------------------'
+provincefile = open("provincedata", "r")
+provincedata = provincefile.readlines()
+ppseats = 0
+psoeseats = 0
+upseats = 0
+csseats = 0
+voxseats = 0
+otherseats = 0
+for line in provincedata:
+    splitLine = line.rstrip().split()
+    newProvince = Province(splitLine, percentages)
+    ppseats += newProvince.ppseats
+    psoeseats += newProvince.psoeseats
+    upseats += newProvince.upseats
+    csseats += newProvince.csseats
+    voxseats += newProvince.voxseats
+    otherseats += newProvince.otherseats
+print '-----------------------'
+print "Partido Popular: " + results.ppstring
+print "Partido Socialista Obrero Espanol: " + results.psoestring
+print "Unidas Podemos: " + results.upstring
+print "Ciudadanos: " + results.csstring
+print "Vox: " + results.voxstring
+print '-----------------------'
+print "Partido Popular: " + str(ppseats)
+print "Partido Socialista Obrero Espanol: " + str(psoeseats)
+print "Unidas Podemos: " + str(upseats)
+print "Ciudadanos: " + str(csseats)
+print "Vox: " + str(voxseats)
+print "Otros: " + str(otherseats)
+print '-----------------------'
+print 'PP + Ciudadanos + Vox: ' + str(ppseats + csseats + voxseats)
+print 'PSOE + Unidas Podemos: ' + str(psoeseats + upseats)
+print 'PSOE + Unidas Podemos + Otros: ' + str(psoeseats + upseats + otherseats)
+print 'PSOE + Ciudadanos: ' + str(psoeseats + csseats)
